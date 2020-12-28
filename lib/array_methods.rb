c974751c0d25258c2b8891fc28eb8a0696a80814
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-28 20:52:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,8 @@ def using_include(array, element = "wow")
 end
 
 def using_sort(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  using_sort.sort
 end
 
 def using_reverse(array)
